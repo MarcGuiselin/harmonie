a61beef4d8e6418c3798f65d4a66953b168dfaaa
Marc Guiselin <marc@guiselin.com>
2024-09-29 18:53:35 +0200

    Improved safety of wasm pointer code

--- crates/bevy_harmonize/common/src/utils.rs
+++ crates/bevy_harmonize/common/src/utils.rs
@@ -1,26 +1,34 @@
 use std::ops::Range;
 
+/// Valid only for 32-bit wasm!
 #[derive(Debug, Clone, Copy)]
-pub struct WasmPointer {
+pub struct WasmPointer<'a> {
+    #[cfg(not(target_arch = "wasm32"))]
+    ptr: u32,
+    #[cfg(target_arch = "wasm32")]
     pub ptr: u32,
+    #[cfg(not(target_arch = "wasm32"))]
+    len: u32,
+    #[cfg(target_arch = "wasm32")]
     pub len: u32,
+    _marker: std::marker::PhantomData<&'a ()>,
 }
 
-impl Into<WasmPointer> for u64 {
-    fn into(self) -> WasmPointer {
-        let ptr = self as u32;
-        let len = (self >> 32) as u32;
-        WasmPointer::new(ptr, len)
+impl From<u64> for WasmPointer<'_> {
+    fn from(value: u64) -> Self {
+        let ptr = value as u32;
+        let len = (value >> 32) as u32;
+        Self::new(ptr, len)
     }
 }
 
-impl Into<u64> for WasmPointer {
+impl Into<u64> for WasmPointer<'_> {
     fn into(self) -> u64 {
         (self.len as u64) << 32 | self.ptr as u64
     }
 }
 
-impl Into<Range<u64>> for WasmPointer {
+impl Into<Range<u64>> for WasmPointer<'_> {
     fn into(self) -> Range<u64> {
         let start = self.ptr as u64;
         let end = start + self.len as u64;
@@ -28,17 +36,18 @@ impl Into<Range<u64>> for WasmPointer {
     }
 }
 
-impl WasmPointer {
-    pub fn new(ptr: u32, len: u32) -> Self {
-        Self { ptr, len }
+impl<'a> WasmPointer<'a> {
+    fn new(ptr: u32, len: u32) -> Self {
+        Self {
+            ptr,
+            len,
+            _marker: std::marker::PhantomData,
+        }
     }
 
-    /// SAFETY: user must ensure sure that value is not mutated or dropped
-    // TODO: Make safe, by keeping a lifetime
-    pub unsafe fn from_vec(vec: &Vec<u8>) -> Self {
-        Self {
-            ptr: vec.as_ptr() as u32,
-            len: vec.len() as u32,
-        }
+    pub fn from_vec(vec: &'a Vec<u8>) -> Self {
+        let ptr = vec.as_ptr();
+        let len = vec.len();
+        Self::new(ptr as _, len as _)
     }
 }
