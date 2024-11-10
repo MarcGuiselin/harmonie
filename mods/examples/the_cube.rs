296d62b2fa01fa848e6c3e602eed49b59401acbb
Marc Guiselin <marc@guiselin.com>
2024-11-10 18:40:47 +0100

    New example mod

---
+++ mods/examples/the_cube.rs
@@ -0,0 +1,34 @@
+use harmony_mod::prelude::*;
+
+init! {
+    Mod::new("The cube")
+        .add_systems(Start, start)
+        .add_systems(Update, update)
+}
+
+#[derive(Component)]
+pub struct Cube;
+
+fn start(
+    mut commands: Commands,
+    //mut meshes: ResMut<Assets<Mesh>>,
+    //mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let entity = commands
+        .spawn_empty()
+        //.insert_component(Transform)
+        .insert_component(Cube)
+        .id();
+    println!("Summoned a new entity {:?} with ", entity);
+}
+
+fn update() {
+    println!("TODO: spin the cube");
+}
+
+// From bevy's `examples\3d\3d_shapes.rs`
+// fn update(mut query: Query<&mut Transform, With<Cube>>, time: Res<Time>) {
+//     for mut transform in &mut query {
+//         transform.rotate_y(time.delta_seconds() / 2.);
+//     }
+// }
