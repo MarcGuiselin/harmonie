eb219bf777b4364586c16b8c638703a340ed4c97
Marc Guiselin <marc@guiselin.com>
2024-11-10 18:38:46 +0100

    Client should not be in charge of building mods

--- crates/harmony_client/src/main.rs
+++ crates/harmony_client/src/main.rs
@@ -1,32 +1,14 @@
-use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*, tasks::AsyncComputeTaskPool};
-use std::{io::Result, time::Duration};
+use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*};
+use std::time::Duration;
 
 fn main() {
     App::new()
         .add_plugins((
-            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs(1))),
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_millis(
+                1000 / 60,
+            ))),
             bevy_harmonize::ModloaderPlugin,
             LogPlugin::default(),
         ))
-        .add_systems(Startup, build_mods)
         .run();
-}
-
-fn build_mods() {
-    async fn build() -> Result<()> {
-        let debug = cfg!(debug_assertions);
-        let directory = std::env::current_dir()?;
-        let packages = vec!["the_cube".into()];
-        bevy_harmonize_build::build(!debug, directory, packages).await?;
-
-        Ok(())
-    }
-
-    AsyncComputeTaskPool::get()
-        .spawn(async {
-            if let Err(e) = build().await {
-                error!("Failed to build mods: {:?}", e);
-            }
-        })
-        .detach();
 }
