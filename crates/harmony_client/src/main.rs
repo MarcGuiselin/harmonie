e6d2dbb7cbd0683638ae200c16fc56dc5add6f05
Marc Guiselin <marc@guiselin.com>
2024-11-11 20:03:12 +0100

    Fix bevy_task not working correctly

--- crates/harmony_client/src/main.rs
+++ crates/harmony_client/src/main.rs
@@ -1,14 +1,7 @@
-use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*};
-use std::time::Duration;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins((
-            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_millis(
-                1000 / 60,
-            ))),
-            bevy_harmonize::ModloaderPlugin,
-            LogPlugin::default(),
-        ))
+        .add_plugins((DefaultPlugins, bevy_harmonize::ModloaderPlugin))
         .run();
 }
